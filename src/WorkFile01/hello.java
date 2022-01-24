0c9a44535268b0d65acf14f30a2ca05a407c259f
brandoncabrera <brandonfcab@gmail.com>
2022-01-24 16:41:09 -0500

    added local to remote repository

--- src/WorkFile01/hello.java
+++ src/WorkFile01/hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello work file 01");
         System.out.println("Created git local repository");
+        System.out.println("local repository was pushed to Github remote repository");
+        System.out.println("Adding another change");
     }
 }
