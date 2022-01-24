ffb1bb6eb9f0b055311647879bbb5cd27b3d6bcd
brandoncabrera <brandonfcab@gmail.com>
2022-01-24 12:46:51 -0500

    Added more work

--- src/WorkFile01/hello.java
+++ src/WorkFile01/hello.java
@@ -3,5 +3,6 @@ package WorkFile01;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello work file 01");
+        System.out.println("Created git local repository");
     }
 }
