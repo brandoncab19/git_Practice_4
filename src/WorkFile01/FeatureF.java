4f93f3faa7524cc3e1e76ffea5536bfdc7b11214
brandoncabrera <brandonfcab@gmail.com>
2022-01-29 15:04:05 -0500

    Did the "Accept yours" option

--- src/WorkFile01/FeatureF.java
+++ src/WorkFile01/FeatureF.java
@@ -4,4 +4,8 @@ public class FeatureF {
 
     //Other work feature-F doesn't know about
 
+    /* "Accept yours" option, overwrites your work created from the
+    master branch instead of the work from created branch
+     */
+
 }
