54dad3eadc92de0b34e02a4a902ac11d7adc4dd3
brandoncabrera <brandonfcab@gmail.com>
2022-01-29 15:57:27 -0500

    FeatureG merge conflict resolved

--- src/WorkFile01/FeatureG.java
+++ src/WorkFile01/FeatureG.java
@@ -2,11 +2,15 @@ package WorkFile01;
 
 public class FeatureG {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Bing");
         System.out.println("....");
+                // above was the master branch
+
         System.out.println("Bong");
-=======
+                // this was the featured branch
+
+        // merged and resolved 2 conflicted commits
 
     }
 }
